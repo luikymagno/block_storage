a8f357c2d027b230c57a7fbd9b13a49306091d55
Luiky Magno <luiky.magno@lsbd.ufc.br>
2018-07-11 16:46:47 -0300

    Working on DiskLog

--- src/disk_log.cpp
+++ src/disk_log.cpp
@@ -1,6 +1,17 @@
 #include "disk_log.h"
 
-DiskLog::DiskLog()
-{
+DiskLog::DiskLog() : i(0) {
+
+}
+
+void DiskLog::addLogEntry(LogEntry logE) {
+    logB.entries[i++] = logE;
+    if(i==200) {
+        write();
+        i=0;
+    }
+}
+
+void DiskLog::write() {
 
 }
